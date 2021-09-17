4f179ea07e709953d1c5a3fecc28a4c77bc3f8de
nicolafelice <felicenicola99@hotmail.com>
2021-09-17 15:44:05 +0200

    vue slider

---
+++ app.js
@@ -0,0 +1,55 @@
+// ricrea slider 
+// aggiungi auto slide every 3 s
+
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        sliderImages: [
+            {
+                url: "https://cdn.pixabay.com/photo/2016/10/14/19/21/canyon-1740973_1280.jpg",
+                nameImg: 'canyon',
+                alt: 'canyon image' 
+            },
+            {
+                url: "https://cdn.pixabay.com/photo/2013/04/04/12/34/mountains-100367_1280.jpg",
+                nameImg: 'mountains sunset',
+                alt: 'mountains sunset image' 
+            },
+            {
+                url: "https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_1280.jpg",
+                nameImg: 'mountain landscape',
+                alt: 'mountain landscape image' 
+            },
+            {
+                url: "https://cdn.pixabay.com/photo/2016/08/09/21/54/lake-1581879_1280.jpg",
+                nameImg: 'lake',
+                alt: 'lake image' 
+            },            
+        ],
+
+        imageIndex: 0,
+    }, 
+    methods: {
+        prevImage: function() {            
+            if ( this.imageIndex == 0 ) {
+                this.imageIndex = this.sliderImages.length - 1;
+            } else {
+                this.imageIndex--;
+            }
+        },
+
+        nextImage: function() {
+            if ( this.imageIndex == this.sliderImages.length - 1 ) {
+                this.imageIndex = 0;
+            } else {
+                this.imageIndex++;
+            }
+        },
+
+        changeimage: function(index) {
+            this.imageIndex = index;
+        }
+    }
+
+});
